fcdcb60675c84f2dd867a0937b570d5f408c608a
adrianepedroso <adrianeped1995@gmail.com>
2021-03-29 19:13:17 -0300

    Feat: add  History class

--- desafio-invest/Wallet.cs
+++ desafio-invest/Wallet.cs
@@ -6,7 +6,24 @@ namespace desafio_invest
 {
     public class Wallet
     {
+        public class History {
+            public string Cod;
+            public int Price;
+            public int Quantity;
+            public DateTime BoughtAt;
+            public int InvestedValue;
+
+            public History (string cod, int price, int quantity, DateTime boughtAt) {
+                Cod = cod;
+                Price = price;
+                Quantity = quantity;
+                BoughtAt = boughtAt;
+                InvestedValue = quantity * price;
+            }
+        }
         private Dictionary<string, Asset> assets = new Dictionary<string, Asset>();
+
+        private List<History> history = new List<History>();
         public void AddAsset(string cod, int price, int quantity) {
             if(assets.ContainsKey(cod)){
                 assets[cod].Add(quantity, price);
